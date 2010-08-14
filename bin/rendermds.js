b0b0611727c238292d8ebe3e68049da88a0ca80d
Simon Oberhammer <simon@nekapuzer.at>
2010-08-14 13:36:03 +0200

    add single wiki page renderer for current wiki deployment

--- bin/rendermds.js
+++ bin/rendermds.js
@@ -3,6 +3,7 @@ var {createSkin} = require('ringo/skin');
 var strings = require('ringo/utils/strings');
 var objects = require('ringo/utils/objects');
 
+var ONEPAGE_WIKI = true;
 var outputPath = '../htdocs/';
 
 // input
@@ -12,49 +13,61 @@ var toc = read(join(sectionPath, 'TOC'));
 // output
 var tocSkin = createSkin(read(join(module.directory, '../skins/toc.md')));
 var sectionSkin = createSkin(read(join(module.directory, '../skins/section.md')));
+var onePageSkin = createSkin(read(join(module.directory, '../skins/onepagewiki.md')));
+
 var htdocsPath = join(module.directory, outputPath);
 
 // collect sections to extend them with next/prev link 
-var sections = [];
-toc.split('\n').every(function(tocLine, idx) {
-   // die on empty line
-   if (!tocLine) return false;
-   // ignore comment for now
-   if (strings.startsWith(tocLine, '##')) return true;
-   
-   // extract first heading for toc display
-   var sectionText = read(join(sectionPath, tocLine + '.md'), 'r');
-   var sectionHeader = tocLine; // fallback
-   var candidate;
-   
-   sectionText.split('\n').every(function(sectionLine) {
-      if (strings.startsWith(sectionLine, '-----')) {
-         sectionHeader = candidate;
-         return false;
+
+function getSections(toc) {
+   var sections = [];
+   toc.split('\n').every(function(tocLine, idx) {
+      // die on empty line
+      if (!tocLine) return false;
+      // ignore comment for now
+      if (strings.startsWith(tocLine, '##')) return true;
+      
+      // extract first heading for toc display
+      var sectionText = read(join(sectionPath, tocLine + '.md'), 'r');
+      var sectionHeader = tocLine; // fallback
+      var candidate;
+      
+      sectionText.split('\n').every(function(sectionLine) {
+         if (strings.startsWith(sectionLine, '-----')) {
+            sectionHeader = candidate;
+            return false;
+         }
+         candidate = sectionLine;
+         return true;
+      });
+      var section = {
+         file: tocLine + '.md',
+         shortTitle: tocLine,
+         title: sectionHeader,
+         text: sectionText,
+      };
+      if (idx > 0) {
+         section.previous = sections[sections.length-1];
       }
-      candidate = sectionLine;
+      if (idx > 1) {
+         sections[sections.length-1].next = section;
+      }
+      sections.push(section);
       return true;
    });
-   var section = {
-      file: tocLine + '.md',
-      title: sectionHeader,
-      text: sectionText,
-   };
-   if (idx > 0) {
-      section.previous = sections[sections.length-1];
-   }
-   if (idx > 1) {
-      sections[sections.length-1].next = section;
-   }
-   sections.push(section);
-   return true;
-});
+   return sections;
+}
 
-// write new md files to htdocs
+var sections = getSections(toc);
 var tocContext = objects.merge(require('ringo/skin/macros'),{sections:sections});
-var tocText = tocSkin.render(tocContext);
-write(join(htdocsPath, 'index.md'), tocText);
-sections.forEach(function(section) {
-   write(join(htdocsPath, section.file), sectionSkin.render({section: section}));
-});
 
+if (ONEPAGE_WIKI == true) {
+   var fullText = onePageSkin.render(tocContext);
+   write(join(htdocsPath, 'index.md'), fullText);
+} else {
+   var tocText = tocSkin.render(tocContext);
+   write(join(htdocsPath, 'index.md'), tocText);
+   sections.forEach(function(section) {
+      write(join(htdocsPath, section.file), sectionSkin.render({section: section}));
+   });
+}
